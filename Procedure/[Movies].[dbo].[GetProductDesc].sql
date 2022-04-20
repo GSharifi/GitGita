bd81bd1f29408f110dcb5853ebaa2cd1d56b7185
Git <gita.sharifi@quest.com>
2022-04-20 09:26:36 -0700

    Version 1 added comments

---
+++ Procedure/[Movies].[dbo].[GetProductDesc].sql
@@ -0,0 +1,22 @@
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[GetProductDesc]
+WITH EXEC AS CALLER
+AS
+BEGIN
+SET NOCOUNT ON
+ 
+/***vesion 1**/
+ 
+SELECT Product_ID, Product, Price, Category, Manufacturer_ID 
+FROM Movies.dbo.Products
+ 
+END
+GO
+GRANT ALTER
+ON OBJECT::[dbo].[GetProductDesc]
+TO [guest]
+WITH GRANT OPTION
+AS [dbo]
+GO
